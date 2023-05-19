f2aaee4f688bb01ac66c82a846185750b77ef89a
Evan Ray <elray@umass.edu>
2023-05-19 11:59:02 -0400

    add tests for make_d_fn related to single unique q

--- tests/testthat/test-make_d_fn.R
+++ tests/testthat/test-make_d_fn.R
@@ -1,6 +1,7 @@
-test_that("make_q_fn works, all qs duplicated", {
-    Q <- make_q_fn(ps = c(.25, .5, .75), qs = c(0, 0, 0))
-    
-    expect_equal(Q(0.5), 0.0)
-    expect_equal(Q(c(0.01, 0.25, 0.29, 0.5)), rep(0.0, 4))
+test_that("make_d_fn throws error, all qs duplicated", {
+    expect_error(make_d_fn(ps = c(.25, .5, .75), qs = c(0, 0, 0)))
+})
+
+test_that("make_d_fn throws error, only one q provided", {
+    expect_error(make_d_fn(ps = .5, qs = 0))
 })
